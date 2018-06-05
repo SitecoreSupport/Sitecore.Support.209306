df3b3dd47c5ac6218a2afbc180b72f3ac572ca7e
DK\ALTU <altu@sitecore.net>
2018-06-05 15:10:55 +0300

    Fixed 209306

---
+++ src/Sitecore.Support.209306/ServicesConfigurator.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.Support.Owin.Authentication.Services
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.DependencyInjection;
+  using System.Diagnostics.CodeAnalysis;
+
+  public class ServicesConfigurator : IServicesConfigurator
+  {
+    [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddSingleton<ApplicationUserResolver, Sitecore.Support.Owin.Authentication.Services.DefaultApplicationUserResolver>();
+    }
+  }
+}
